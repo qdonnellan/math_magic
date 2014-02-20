3cd6908aed0d6359cb96e1f1467e2a5846bc5dbd
qdonnellan <qdonnellan@gmail.com>
2014-02-20 06:57:25 -0600

    added sandbox file (my current musings/barebones for this project)

---
+++ math_magic/sandbox.py
@@ -0,0 +1,22 @@
+import re
+import random
+
+# just a sandbox for trying things out
+
+prb = '(1,4]**2 + [1,4]**2'
+
+pattern = re.compile("[\(\[]\d+,\d+[\)\]]")
+results = re.findall(pattern, prb)
+
+for result in results:
+    a = int(re.search('(\d+),', result).group(1))
+    b = int(re.search(',(\d+)', result).group(1))
+    if '(' in result: 
+        a += 1
+    if ')' in result: 
+        b += 1
+    r = random.randint(a,b)
+    prb = re.sub(pattern, str(r), prb, count=1)
+
+print prb
+
